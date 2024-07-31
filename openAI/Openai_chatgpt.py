21badbb0d6360bf1c2227fb068fc4d6e7ad23f99
Victoria.White <victoria91718@gmail.com>
2024-07-31 12:00:55 +0800

    Openai_chatgpt updated

--- openAI/Openai_chatgpt.py
+++ openAI/Openai_chatgpt.py
@@ -5,7 +5,7 @@ client = OpenAI(api_key = api_key)
 
 conversation = [
     {"role": "assistant", "content": "你是一個專業的關鍵字分析師，會根據這16類對問題進行分析，16類有:用電大戶、光儲合一、市場資訊、E-dReg、sReg、dReg、即時備轉、補充備轉、創新能源技術、電價方案、再生能源、台電說明會、規範解析和台電供需資訊，請根據問題分析該問題符合這16類中的哪幾類，使用繁體中文回覆"},
-    {"role": "user", "content": "我想知道如何計算家中電費?"}, # 跟我介紹一下E-dReg的規範？ 幫我說明目前sReg價金的計算方式？
+    {"role": "user", "content": "我想知道如何計算家中電費?"}, # ex1:跟我介紹一下E-dReg的規範？ ex2:幫我說明目前sReg價金的計算方式？
     {"role": "assistant", "content": "請只回覆關鍵字"}
 ]
 
