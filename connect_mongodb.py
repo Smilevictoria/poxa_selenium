e8add712eb3c397535b49ff984949e3451795a83
Victoria.White <victoria91718@gmail.com>
2024-08-14 11:50:54 +0800

    mongodb find section

--- connect_mongodb.py
+++ connect_mongodb.py
@@ -4,7 +4,6 @@ from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://victoria91718:white0718@poxa.1j2eh.mongodb.net/?retryWrites=true&w=majority&appName=poxa"
 client = pymongo.MongoClient(uri)
-# client = pymongo.MongoClient(uri, server_api=ServerApi('1'))
 
 mydb = client["Test"]
 mycol = mydb["info"]
@@ -15,12 +14,22 @@ mycol = mydb["info"]
 # x = mycol.insert_many(existing_data)
 # print("finsh~")
 
+# Can use in "title/content"
 find_result1 = list(mycol.find({'name': 'Victoria'}))
 print(find_result1[0]['id'])
 
+# Can use in "labels/subtitle/subcontent"
 # get all article with '市場資訊' of labels
 find_result2 = list(mycol.find({}))
 for document in find_result2:
     labels = document.get('labels', {})
     if '市場資訊' in labels.values():
         print(document['title'])
+
+# Can use in "section"
+find_result3 = list(mycol.find({}))
+for document in find_result3:
+    section = document.get('section', {})
+    if any('颱風' in item for sublist in section.values() for item in sublist):
+        print(document.get('title', 'No title'))
+
