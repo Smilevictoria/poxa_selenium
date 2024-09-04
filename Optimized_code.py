02a1eea2e96c13f0660ce46e6efea884ef67ce71
Victoria.White <victoria91718@gmail.com>
2024-09-04 14:52:38 +0800

    Get 2 new articles

--- Optimized_code.py
+++ Optimized_code.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-import pymongo, json
+import pymongo
 from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://victoria91718:white0718@poxa.1j2eh.mongodb.net/?retryWrites=true&w=majority&appName=poxa"
