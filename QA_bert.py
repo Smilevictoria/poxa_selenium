8a1552a04c402bc3fbd06be88985400cf86fa4db
Victoria.White <victoria91718@gmail.com>
2024-09-05 16:27:53 +0800

    Update QA_bert

--- QA_bert.py
+++ QA_bert.py
@@ -16,15 +16,17 @@ client = OpenAI(api_key = api_key)
 def text_embedding(text):
     return model.encode(text)
 
+# def weighted_embedding(embeddings, weights):
+#     weighted_emb = sum(emb * weight for emb, weight in zip(embeddings, weights)) / sum(weights)
+#     return weighted_emb
+
 def article_text_embedding():
     datas = list(mycol.find({}))
     data_embedding = []
 
     for data in datas:
         combined_content = ""
-        combined_content += f"標題: {data['title']}\n"
-        combined_content += f"內容: {data['content']}\n"
-        
+
         for i, block in data['block'].items():
             combined_content += f"段落內容: {block['blockContent']}\n"
         
@@ -35,6 +37,39 @@ def article_text_embedding():
         article_embedding = text_embedding(combined_content)
         data_embedding.append((combined_content, article_embedding))
     return data_embedding
+    # for data in datas:
+    #     title_weight = 0.1 
+    #     content_weight = 0.2 
+    #     block_weight = 0.4
+    #     section_weight = 0.3
+
+    #     title_embedding = text_embedding(f"標題: {data['title']}")
+    #     content_embedding = text_embedding(f"內容: {data['content']}")
+
+    #     block_embeddings = []
+    #     for i, block in data['block'].items():
+    #         block_embeddings.append(text_embedding(f"段落內容: {block['blockContent']}"))
+        
+    #     section_embeddings = []
+    #     for i, section in data['section'].items():
+    #         section_embeddings.append(text_embedding(f"部分內容: {section['sectionContent']}"))
+
+    #     embeddings = [title_embedding, content_embedding]
+    #     weights = [title_weight, content_weight]
+        
+    #     for block_emb in block_embeddings:
+    #         embeddings.append(block_emb)
+    #         weights.append(block_weight)
+        
+    #     for section_emb in section_embeddings:
+    #         embeddings.append(section_emb)
+    #         weights.append(section_weight)
+
+    #     article_embedding = weighted_embedding(embeddings, weights)
+    #     combined_content = f"標題: {data['title']}\n內容: {data['content']}\n"
+    #     data_embedding.append((combined_content, article_embedding))
+
+    # return data_embedding
 
 def cosine_similarity(embedding1, embedding2):
     return np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))
@@ -64,7 +99,7 @@ def generate_response(question, rel_content):
 start_time = time.time()
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-user_input = "目前調頻備轉價格是多少？" # 幫我說明目前sReg價金的計算方式？ 光儲的參與規則？ 我有1MW的光電案場，可以蓋多大的儲能案場？收益大概如何？
+user_input = "目前調頻備轉的參與容量是多少MW？" # 幫我說明目前sReg價金的計算方式？  我有1MW的光電案場，可以蓋多大的儲能案場？收益大概如何？光儲的參與規則？
 qa_embedding = text_embedding(user_input)
 article_embedding = article_text_embedding()
 relevant_content = find_most_relevant(qa_embedding, article_embedding)
